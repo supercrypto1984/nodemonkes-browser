76571f849bc02e85f1105d5b0664a6bdd33ff289
supercrypto1984 <85349327+supercrypto1984@users.noreply.github.com>
2024-12-13 21:40:57 +0800

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MonkesList from './components/MonkesList'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Node Monkes Browser</h1>
+      <MonkesList />
+    </div>
+  )
+}
+
+export default App
+
