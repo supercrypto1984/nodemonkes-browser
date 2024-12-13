e058274680ef7803367a7346353078e8001113e3
supercrypto1984 <85349327+supercrypto1984@users.noreply.github.com>
2024-12-13 21:36:11 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/nodemonkes-browser/',
   build: {
     outDir: 'dist',
-    sourcemap: true
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
   }
 })
 
