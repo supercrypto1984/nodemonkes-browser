12a95b12f2995233823cff6d1297abaeef507741
supercrypto1984 <85349327+supercrypto1984@users.noreply.github.com>
2024-12-13 21:11:03 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nodemonkes-browser/',
+  base: '/nodemonkes-browser/', // 更新为您的仓库名
 })
 
